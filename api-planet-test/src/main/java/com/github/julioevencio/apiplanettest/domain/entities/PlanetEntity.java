092919975dd6ff5ee3ef2f622ff87c0c561163a8
JulioEvencio <julioevencio@acad.ifma.edu.br>
2023-04-08 15:03:23 -0300

    Added ORM

---
+++ api-planet-test/src/main/java/com/github/julioevencio/apiplanettest/domain/entities/PlanetEntity.java
@@ -0,0 +1,70 @@
+package com.github.julioevencio.apiplanettest.domain.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_planet")
+public class PlanetEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, unique = true)
+	private String name;
+
+	@Column(nullable = false)
+	private String climate;
+
+	@Column(nullable = false)
+	private String terrain;
+
+	public PlanetEntity() {
+		// Code
+	}
+
+	public PlanetEntity(Long id, String name, String climate, String terrain) {
+		this.id = id;
+		this.name = name;
+		this.climate = climate;
+		this.terrain = terrain;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getClimate() {
+		return climate;
+	}
+
+	public void setClimate(String climate) {
+		this.climate = climate;
+	}
+
+	public String getTerrain() {
+		return terrain;
+	}
+
+	public void setTerrain(String terrain) {
+		this.terrain = terrain;
+	}
+
+}
