bf7fb10b174fa115b4ca59b591ce92249e30c309
JulioEvencio <julioevencio@acad.ifma.edu.br>
2023-04-08 12:03:31 -0300

    Added Project Base

---
+++ api-planet-test/src/main/java/com/github/julioevencio/apiplanettest/ApiPlanetTestApplication.java
@@ -0,0 +1,13 @@
+package com.github.julioevencio.apiplanettest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPlanetTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPlanetTestApplication.class, args);
+	}
+
+}
---
+++ api-planet-test/src/test/java/com/github/julioevencio/apiplanettest/ApiPlanetTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.julioevencio.apiplanettest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPlanetTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
